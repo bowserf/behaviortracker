9a19169597fff9447d94e531d5041f7da075fd44
bowserf <torcheux.frederic@gmail.com>
2018-01-19 23:05:00 +0100

    Prepare home activitiy

---
+++ app/src/main/java/fr/bowser/behaviortracker/home/HomeActivity.kt
@@ -0,0 +1,13 @@
+package fr.bowser.behaviortracker.home
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import fr.bowser.behaviortracker.R
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
