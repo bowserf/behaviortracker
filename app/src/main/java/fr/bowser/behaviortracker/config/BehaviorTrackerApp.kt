78f22020b88301cfa365da49bbf767900c49560c
bowserf <torcheux.frederic@gmail.com>
2018-01-19 23:11:35 +0100

    Add application to enable strict mode

---
+++ app/src/main/java/fr/bowser/behaviortracker/config/BehaviorTrackerApp.kt
@@ -0,0 +1,25 @@
+package fr.bowser.behaviortracker.config
+
+import android.app.Application
+import android.os.StrictMode
+import fr.bowser.behaviortracker.BuildConfig
+
+
+class BehaviorTrackerApp : Application(){
+
+    override fun onCreate() {
+        if (BuildConfig.DEBUG) {
+            StrictMode.setThreadPolicy(StrictMode.ThreadPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog()
+                    .build())
+            StrictMode.setVmPolicy(StrictMode.VmPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog()
+                    .build())
+        }
+        
+        super.onCreate()
+    }
+
+}
