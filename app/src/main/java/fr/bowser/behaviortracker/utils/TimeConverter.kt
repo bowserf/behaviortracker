dbeb896b63bc9bf50c058a1dccdbbc9b2854d3bb
Torcheux Frédéric <frederic.torcheux@djit.fr>
2018-01-22 10:33:34 +0100

    Add @JvmStatic to keep Timeconverter code in Kotlin instead of Java
    It's only Kotlin tests which don't work because of a problem with AAPT2

---
+++ app/src/main/java/fr/bowser/behaviortracker/utils/TimeConverter.kt
@@ -0,0 +1,36 @@
+package fr.bowser.behaviortracker.utils
+
+object TimeConverter {
+
+    @JvmStatic
+    fun convertSecondsToHumanTime(time: Long): String{
+        var remainingTime = time
+        var string = ""
+
+        val hours = remainingTime / 3600
+
+        if(hours < 10){
+            string = "0"
+        }
+        string = string + hours + ":"
+
+        remainingTime %= 3600
+
+        val minutes = remainingTime / 60
+        if(minutes < 10){
+            string += "0"
+        }
+        string = string + minutes + ":"
+
+        remainingTime %= 60
+
+        val seconds = remainingTime
+        if(seconds < 10){
+            string += "0";
+        }
+        string += seconds;
+
+        return string;
+    }
+
+}
