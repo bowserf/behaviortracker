20ca38e25dea4973cd0ec7aa3062de0242c91bca
bowserf <torcheux.frederic@gmail.com>
2018-01-26 23:36:54 +0100

    Add second constructor with only name and color in parameters

--- app/src/main/java/fr/bowser/behaviortracker/timer/Timer.kt
+++ app/src/main/java/fr/bowser/behaviortracker/timer/Timer.kt
@@ -2,10 +2,15 @@ package fr.bowser.behaviortracker.timer
 
 import android.arch.persistence.room.ColumnInfo
 import android.arch.persistence.room.Entity
+import android.arch.persistence.room.Ignore
 import android.arch.persistence.room.PrimaryKey
 
 @Entity(tableName = "Timer" )
 data class Timer(@PrimaryKey @ColumnInfo(name = "id") var id: Long,
                  @ColumnInfo(name = "current_time") var currentTime: Long,
                  @ColumnInfo(name = "name") val name: String,
-                 @ColumnInfo(name = "color") var color: Int)
+                 @ColumnInfo(name = "color") var color: Int){
+    @Ignore
+    constructor(name: String, color: Int)
+            : this(0, 0, name, color)
+}
