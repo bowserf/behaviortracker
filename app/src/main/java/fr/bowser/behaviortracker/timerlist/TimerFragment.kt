94159debf0211533229bd68c0e5bf91b270bb8ae
bowserf <torcheux.frederic@gmail.com>
2018-01-26 21:38:35 +0100

    Fix imports

--- app/src/main/java/fr/bowser/behaviortracker/timerlist/TimerFragment.kt
+++ app/src/main/java/fr/bowser/behaviortracker/timerlist/TimerFragment.kt
@@ -11,9 +11,6 @@ import android.view.View
 import android.view.ViewGroup
 import fr.bowser.behaviortracker.R
 import fr.bowser.behaviortracker.createtimer.CreateTimerActivity
-import fr.bowser.behaviortracker.home.DaggerTimerComponent
-import fr.bowser.behaviortracker.home.TimerContract
-import fr.bowser.behaviortracker.home.TimerModule
 import fr.bowser.behaviortracker.timer.Timer
 import javax.inject.Inject
 
