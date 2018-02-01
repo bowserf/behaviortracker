e366d2c2080094358664136c05dcde464ef7e7f2
Torcheux Frédéric <frederic.torcheux@djit.fr>
2018-02-01 16:13:15 +0100

    WIP

--- app/src/main/java/fr/bowser/behaviortracker/createtimer/ColorAdapter.kt
+++ app/src/main/java/fr/bowser/behaviortracker/createtimer/ColorAdapter.kt
@@ -1,6 +1,8 @@
 package fr.bowser.behaviortracker.createtimer
 
 import android.content.Context
+import android.graphics.PorterDuff
+import android.graphics.drawable.GradientDrawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,25 +13,27 @@ class ColorAdapter(context: Context?, resource: Int, objects: MutableList<Int>?)
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         val view: View
-        if(convertView == null){
+        if (convertView == null) {
             view = LayoutInflater.from(context).inflate(R.layout.item_color, parent, false)
-        }else{
+        } else {
             view = convertView
         }
         val color = getItem(position)
-        view.setBackgroundColor(color)
+        val background = view.findViewById<View>(R.id.item_color).background as GradientDrawable
+        background.setColorFilter(color, PorterDuff.Mode.SRC_ATOP)
         return view
     }
 
     override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup?): View {
         val view: View
-        if(convertView == null){
-            view = LayoutInflater.from(context).inflate(R.layout.item_color_drop_down, parent, false)
-        }else{
+        if (convertView == null) {
+            view = LayoutInflater.from(context).inflate(R.layout.item_color, parent, false)
+        } else {
             view = convertView
         }
         val color = getItem(position)
-        view.findViewById<View>(R.id.item_color_drop_down_color).setBackgroundColor(color)
+        val background = view.findViewById<View>(R.id.item_color).background as GradientDrawable
+        background.setColorFilter(color, PorterDuff.Mode.SRC_ATOP)
         return view
     }
 
