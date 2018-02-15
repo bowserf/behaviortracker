049b9579fb097817a9d77d041f71448eda9c3e27
Torcheux Frédéric <frederic.torcheux@djit.fr>
2018-02-15 12:04:35 +0100

    Remove bottom navigation view because stat fragment has been removed

--- app/src/main/java/fr/bowser/behaviortracker/home/HomeActivity.kt
+++ app/src/main/java/fr/bowser/behaviortracker/home/HomeActivity.kt
@@ -1,41 +1,22 @@
 package fr.bowser.behaviortracker.home
 
 import android.os.Bundle
-import android.support.design.widget.BottomNavigationView
-import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentTransaction
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
 import fr.bowser.behaviortracker.R
-import fr.bowser.behaviortracker.stat.StatFragment
 import fr.bowser.behaviortracker.timerlist.TimerFragment
-import javax.inject.Inject
 
-class HomeActivity : AppCompatActivity(), HomeContract.View {
-
-    @Inject
-    lateinit var presenter: HomePresenter
+class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        setupGraph()
-
         initializeToolbar()
 
-        initializeBottomBar()
-
-        presenter.start()
-    }
-
-    override fun displayTimerView() {
-        displayFragment(TimerFragment() , TimerFragment.TAG)
-    }
-
-    override fun displayTimelineView() {
-        displayFragment(StatFragment() , StatFragment.TAG)
+        displayTimerFragment()
     }
 
     private fun initializeToolbar() {
@@ -43,27 +24,9 @@ class HomeActivity : AppCompatActivity(), HomeContract.View {
         setSupportActionBar(myToolbar)
     }
 
-    private fun displayFragment(fragment:Fragment, tag: String){
+    private fun displayTimerFragment(){
         supportFragmentManager.inTransaction {
-            replace(R.id.fragment_container, fragment, tag)
-        }
-    }
-
-    private fun setupGraph(){
-        val build = DaggerHomeComponent.builder()
-                .homeModule(HomeModule(this))
-                .build()
-        build.inject(this)
-    }
-
-    private fun initializeBottomBar() {
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
-        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.bottom_navigation_timer -> presenter.onClickTimer()
-                R.id.bottom_navigation_timeline -> presenter.onClickTimeline()
-            }
-            true
+            replace(R.id.fragment_container, TimerFragment(), TimerFragment.TAG)
         }
     }
 
