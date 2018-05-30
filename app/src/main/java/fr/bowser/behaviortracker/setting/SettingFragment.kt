a75343f6b388c77e874e1b4e1137011606345cc2
bowserf <torcheux.frederic@gmail.com>
2018-05-30 23:36:50 +0200

    [Setting] Add send commentary feature

--- app/src/main/java/fr/bowser/behaviortracker/setting/SettingFragment.kt
+++ app/src/main/java/fr/bowser/behaviortracker/setting/SettingFragment.kt
@@ -1,7 +1,12 @@
 package fr.bowser.behaviortracker.setting
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.preference.Preference
 import android.preference.PreferenceFragment
+import android.support.annotation.StringRes
+import android.widget.Toast
 import fr.bowser.behaviortracker.R
 import fr.bowser.behaviortracker.config.BehaviorTrackerApp
 import javax.inject.Inject
@@ -12,6 +17,8 @@ class SettingFragment : PreferenceFragment() {
     @Inject
     lateinit var presenter: SettingPresenter
 
+    private lateinit var sendCommentary: Preference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         addPreferencesFromResource(R.xml.preference_list)
@@ -19,6 +26,9 @@ class SettingFragment : PreferenceFragment() {
         setupGraph()
 
         presenter.start()
+
+        sendCommentary = findPreference(R.string.pref_key_send_commentary)
+        sendCommentary.onPreferenceClickListener = onPreferenceClickListener
     }
 
     override fun onDestroy() {
@@ -32,6 +42,41 @@ class SettingFragment : PreferenceFragment() {
                 .settingPresenterModule(SettingPresenterModule())
                 .build()
         component.inject(this)
+    }
+
+    private fun sendCommentary() {
+        val intent = Intent(Intent.ACTION_SENDTO)
+        intent.data = Uri.parse("mailto:$SUPPORT_EMAIL")
+        intent.putExtra(Intent.EXTRA_SUBJECT,
+                resources.getString(R.string.settings_send_email_subject))
+        try {
+            startActivity(Intent.createChooser(
+                    intent,
+                    resources.getString(R.string.settings_send_email_choose_app)))
+        } catch (ex: android.content.ActivityNotFoundException) {
+            Toast.makeText(activity!!,
+                    resources.getString(R.string.settings_send_email_no_application_available),
+                    Toast.LENGTH_SHORT)
+                    .show()
+        }
+    }
+
+    private fun findPreference(@StringRes prefKey: Int): Preference {
+        return findPreference(resources.getString(prefKey))
+    }
+
+    private val onPreferenceClickListener = Preference.OnPreferenceClickListener { preference ->
+        when (preference) {
+            sendCommentary -> {
+                sendCommentary()
+                return@OnPreferenceClickListener true
+            }
+        }
+        false
+    }
+
+    companion object {
+        const val SUPPORT_EMAIL = "torcheux.frederic@gmail.com"
     }
 
 }
