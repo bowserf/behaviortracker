939f260cfdab05f23341779f21991aa4d3203e76
Torcheux Frédéric <frederic.torcheux@djit.fr>
2018-07-19 11:20:53 +0200

    [UA] Add 2 more timers in UA mode

--- app/src/main/java/fr/bowser/behaviortracker/timer/TimerDaoUA.kt
+++ app/src/main/java/fr/bowser/behaviortracker/timer/TimerDaoUA.kt
@@ -13,6 +13,8 @@ class TimerDaoUA(context: Context) : TimerDAO {
         timers.add(Timer(0, 5304L, context.resources.getString(R.string.release_screenshot_timer_work), ColorUtils.convertPositionToColor(10), 0))
         timers.add(Timer(1, 1638L, context.resources.getString(R.string.release_screenshot_timer_transport), ColorUtils.convertPositionToColor(7), 1))
         timers.add(Timer(2, 2700L, context.resources.getString(R.string.release_screenshot_timer_sport), ColorUtils.convertPositionToColor(4), 2))
+        timers.add(Timer(3, 4000L, context.resources.getString(R.string.release_screenshot_timer_cooking), ColorUtils.convertPositionToColor(0), 3))
+        timers.add(Timer(4, 2000L, context.resources.getString(R.string.release_screenshot_timer_watch_series), ColorUtils.convertPositionToColor(18), 4))
     }
 
     override fun getTimers(): List<Timer> {
