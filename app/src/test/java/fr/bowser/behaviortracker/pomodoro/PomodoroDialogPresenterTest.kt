d8836a3210167efc906958ca749eaabc35290a70
bowserf <torcheux.frederic@gmail.com>
2018-11-04 16:48:34 +0100

    [PomodoroDialogPresenter] Add unit test

---
+++ app/src/test/java/fr/bowser/behaviortracker/pomodoro/PomodoroDialogPresenterTest.kt
@@ -0,0 +1,39 @@
+package fr.bowser.behaviortracker.pomodoro
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class PomodoroDialogPresenterTest {
+
+    @Mock
+    private lateinit var manager: PomodoroManager
+
+    @Test
+    fun clickOnPositionButtonResumeSession(){
+        // Given
+        val presenter = PomodoroDialogPresenter(manager)
+
+        // When
+        presenter.onClickPositionButton()
+
+        // Then
+        Mockito.verify(manager).resume()
+    }
+
+    @Test
+    fun clickOnNegativeButtonStopSession(){
+        // Given
+        val presenter = PomodoroDialogPresenter(manager)
+
+        // When
+        presenter.onClickNegativeButton()
+
+        // Then
+        Mockito.verify(manager).stop()
+    }
+
+}
