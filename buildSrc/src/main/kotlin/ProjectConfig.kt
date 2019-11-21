42b9d98282191988c2b22031ee8e76fa9f846985
Frédéric Torcheux <frederic.torcheux@djit.fr>
2019-11-21 16:51:35 +0100

    [Pomodoro] Fix layout for small devices and landscape layout

--- buildSrc/src/main/kotlin/ProjectConfig.kt
+++ buildSrc/src/main/kotlin/ProjectConfig.kt
@@ -1,8 +1,8 @@
 object ProjectConfig {
 
     object SdkVersions {
-        const val versionCode = 70000006
-        const val versionName = "0.7.0"
+        const val versionCode = 70000007
+        const val versionName = "0.7.1"
         const val compileSdkVersion = 29
         const val buildToolsVersion = "29.0.2"
         const val targetSdkVersion = 29
