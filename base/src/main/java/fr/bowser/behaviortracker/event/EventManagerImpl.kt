d41c393ff5814e111a7413e21c3e48e2202cd87b
Frédéric Torcheux <frederic.torcheux@djit.fr>
2019-11-27 17:14:55 +0100

    [Event] Add user property to know if application is an instant app

--- base/src/main/java/fr/bowser/behaviortracker/event/EventManagerImpl.kt
+++ base/src/main/java/fr/bowser/behaviortracker/event/EventManagerImpl.kt
@@ -3,10 +3,15 @@ package fr.bowser.behaviortracker.event
 import android.content.Context
 import android.os.Bundle
 import com.google.firebase.analytics.FirebaseAnalytics
+import fr.bowser.behaviortracker.instantapp.InstantAppManager
 
-class EventManagerImpl(context: Context) : EventManager {
+class EventManagerImpl(context: Context, instantAppManager: InstantAppManager) : EventManager {
 
     private val firebaseAnalytics: FirebaseAnalytics = FirebaseAnalytics.getInstance(context)
+
+    init {
+        firebaseAnalytics.setUserProperty(USER_PROPERTY_IS_INSTANT_APP, instantAppManager.isInstantApp().toString())
+    }
 
     override fun sendTimerCreateEvent(startNow: Boolean) {
         val bundle = Bundle()
@@ -47,6 +52,8 @@ class EventManagerImpl(context: Context) : EventManager {
     }
 
     companion object {
+        const val USER_PROPERTY_IS_INSTANT_APP = "is_instant_app"
+
         const val EVENT_ALARM_NOTIF_CLICKED = "click_alarm_notif"
 
         const val EVENT_ALARM_NOTIF_DISPLAYED = "display_alarm_notif"
--- base/src/main/java/fr/bowser/behaviortracker/event/EventManagerModule.kt
+++ base/src/main/java/fr/bowser/behaviortracker/event/EventManagerModule.kt
@@ -3,6 +3,7 @@ package fr.bowser.behaviortracker.event
 import android.content.Context
 import dagger.Module
 import dagger.Provides
+import fr.bowser.behaviortracker.instantapp.InstantAppManager
 import javax.inject.Singleton
 
 @Module
@@ -10,7 +11,7 @@ class EventManagerModule {
 
     @Singleton
     @Provides
-    fun provideEventManager(context: Context): EventManager {
-        return EventManagerImpl(context)
+    fun provideEventManager(context: Context, instantAppManager: InstantAppManager): EventManager {
+        return EventManagerImpl(context, instantAppManager)
     }
 }
