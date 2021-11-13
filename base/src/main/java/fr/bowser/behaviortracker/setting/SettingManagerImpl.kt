cda5fbc9401f4bfde021a3cb4c5b3f606ce2f50d
Frédéric Torcheux <frederic.torcheux@djit.fr>
2021-11-13 19:16:16 +0100

    [Setting] Pass StringManager instead of a Context to get strings

--- base/src/main/java/fr/bowser/behaviortracker/setting/SettingManagerImpl.kt
+++ base/src/main/java/fr/bowser/behaviortracker/setting/SettingManagerImpl.kt
@@ -4,11 +4,18 @@ import android.content.Context
 import android.content.SharedPreferences
 import android.preference.PreferenceManager
 import fr.bowser.behaviortracker.R
+import fr.bowser.feature_string.StringManager
 
-class SettingManagerImpl(private val context: Context) : SettingManager,
-    SharedPreferences.OnSharedPreferenceChangeListener {
+class SettingManagerImpl(
+    private val context: Context,
+    private val stringManager: StringManager
+) : SettingManager {
 
-    private var timerModificationListener = mutableSetOf<SettingManager.TimerModificationListener>()
+    private val timerModificationListener = mutableSetOf<SettingManager.TimerModificationListener>()
+
+    private val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
+
+    private val sharedPreferenceChangeListener = createSharedPreferenceChangeListener()
 
     private var timeModification: Int
 
@@ -18,61 +25,67 @@ class SettingManagerImpl(private val context: Context) : SettingManager,
 
     private var pomodoroStep: Int
 
-    private val sharedPreferences: SharedPreferences =
-        PreferenceManager.getDefaultSharedPreferences(context)
-
     init {
-        sharedPreferences.registerOnSharedPreferenceChangeListener(this)
+        sharedPreferences.registerOnSharedPreferenceChangeListener(sharedPreferenceChangeListener)
 
         timeModification = sharedPreferences.getInt(
-            context.getString(R.string.pref_key_time_modification),
+            stringManager.getString(R.string.pref_key_time_modification),
             context.resources.getInteger(R.integer.settings_default_value_time_modification_seconds)
         )
 
         pomodoroVibrationEnable = sharedPreferences.getBoolean(
-            context.getString(R.string.pref_key_pomodoro_vibration),
+            stringManager.getString(R.string.pref_key_pomodoro_vibration),
             true
         )
 
         pomodoroPauseStep = sharedPreferences.getInt(
-            context.getString(R.string.pref_key_pomodoro_pause_stage),
+            stringManager.getString(R.string.pref_key_pomodoro_pause_stage),
             context.resources.getInteger(R.integer.settings_default_value_pomodoro_pause_stage_minutes)
         ) * 60
 
         pomodoroStep = sharedPreferences.getInt(
-            context.getString(R.string.pref_key_pomodoro_stage),
+            stringManager.getString(R.string.pref_key_pomodoro_stage),
             context.resources.getInteger(R.integer.settings_default_value_pomodoro_stage_minutes)
         ) * 60
     }
 
-    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences, key: String) {
-        when (key) {
-            context.getString(R.string.pref_key_time_modification) -> {
-                timeModification = sharedPreferences.getInt(
-                    key,
-                    context.resources.getInteger(
-                        R.integer.settings_default_value_time_modification_seconds
+    private fun createSharedPreferenceChangeListener() =
+        SharedPreferences.OnSharedPreferenceChangeListener { sharedPreferences, key ->
+            when (key) {
+                stringManager.getString(R.string.pref_key_time_modification) -> {
+                    timeModification = sharedPreferences.getInt(
+                        key,
+                        context.resources.getInteger(
+                            R.integer.settings_default_value_time_modification_seconds
+                        )
                     )
-                )
-                timerModificationListener.forEach { it.onTimerModificationChanged(timeModification) }
-            }
-            context.getString(R.string.pref_key_pomodoro_vibration) -> {
-                pomodoroVibrationEnable = sharedPreferences.getBoolean(key, true)
-            }
-            context.getString(R.string.pref_key_pomodoro_pause_stage) -> {
-                pomodoroPauseStep = sharedPreferences.getInt(
-                    key, context.resources
-                        .getInteger(R.integer.settings_default_value_pomodoro_pause_stage_minutes)
-                ) * 60
-            }
-            context.getString(R.string.pref_key_pomodoro_stage) -> {
-                pomodoroStep = sharedPreferences.getInt(
-                    key, context.resources
-                        .getInteger(R.integer.settings_default_value_pomodoro_stage_minutes)
-                ) * 60
+                    timerModificationListener.forEach {
+                        it.onTimerModificationChanged(
+                            timeModification
+                        )
+                    }
+                }
+                stringManager.getString(R.string.pref_key_pomodoro_vibration) -> {
+                    pomodoroVibrationEnable = sharedPreferences.getBoolean(key, true)
+                }
+                stringManager.getString(R.string.pref_key_pomodoro_pause_stage) -> {
+                    pomodoroPauseStep = sharedPreferences.getInt(
+                        key,
+                        context.resources.getInteger(
+                            R.integer.settings_default_value_pomodoro_pause_stage_minutes
+                        )
+                    ) * 60
+                }
+                stringManager.getString(R.string.pref_key_pomodoro_stage) -> {
+                    pomodoroStep = sharedPreferences.getInt(
+                        key,
+                        context.resources.getInteger(
+                            R.integer.settings_default_value_pomodoro_stage_minutes
+                        )
+                    ) * 60
+                }
             }
         }
-    }
 
     override fun registerTimerModificationListener(listener: SettingManager.TimerModificationListener) {
         timerModificationListener.add(listener)
--- base/src/main/java/fr/bowser/behaviortracker/setting/SettingManagerModule.kt
+++ base/src/main/java/fr/bowser/behaviortracker/setting/SettingManagerModule.kt
@@ -3,6 +3,7 @@ package fr.bowser.behaviortracker.setting
 import android.content.Context
 import dagger.Module
 import dagger.Provides
+import fr.bowser.feature_string.StringManager
 import javax.inject.Singleton
 
 @Module
@@ -10,7 +11,13 @@ class SettingManagerModule {
 
     @Singleton
     @Provides
-    fun provideSettingManager(context: Context): SettingManager {
-        return SettingManagerImpl(context)
+    fun provideSettingManager(
+        context: Context,
+        stringManager: StringManager
+    ): SettingManager {
+        return SettingManagerImpl(
+            context,
+            stringManager
+        )
     }
 }
