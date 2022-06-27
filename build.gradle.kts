5a7695f7cd0446b90eeaaa3e35d2e2d6da8cfc77
Frédéric Torcheux <frederic.torcheux@djit.fr>
2022-06-28 00:04:33 +0200

    [Gradle] Bump android gradle 7.2.1 and gradle 7.4.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,16 +4,15 @@ buildscript {
 
     repositories {
         google()
-        jcenter()
         maven(url = "https://plugins.gradle.org/m2/")
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.3")
+        classpath("com.android.tools.build:gradle:7.2.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.50")
         classpath("com.google.gms:google-services:4.3.10")
         classpath("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.14.2")
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.3.5")
-        classpath("com.google.firebase:firebase-crashlytics-gradle:2.8.0")
+        classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.0")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle.kts files
