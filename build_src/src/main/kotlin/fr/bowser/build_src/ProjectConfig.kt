7aff250efb9c6a43cac3ee7e1ab278780e3b29ab
Frédéric Torcheux <frederic.torcheux@djit.fr>
2022-10-31 15:41:18 -0400

    [Version] Bump to 1.00.01

--- build_src/src/main/kotlin/fr/bowser/build_src/ProjectConfig.kt
+++ build_src/src/main/kotlin/fr/bowser/build_src/ProjectConfig.kt
@@ -11,8 +11,8 @@ class ProjectConfig : Plugin<Project> {
     }
 
     object SdkVersions {
-        const val versionCode = 70_01_00_00
-        const val versionName = "1.00.00"
+        const val versionCode = 70_01_00_01
+        const val versionName = "1.00.01"
         const val compileSdkVersion = 33
         const val targetSdkVersion = 33
         const val minSdkVersion = 21
