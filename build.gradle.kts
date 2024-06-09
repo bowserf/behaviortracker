9ae544119dd82245dbe0f8ffce7ce613f9504fb5
Frédéric Torcheux <frederic.torcheux@djit.fr>
2024-06-08 23:43:37 -0400

    [Kotlin] Bump to 2.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:8.4.1")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.24")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.0")
         classpath("com.google.gms:google-services:4.3.14")
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.5.3")
         classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.2")
@@ -16,7 +16,7 @@ buildscript {
         // in the individual module build.gradle.kts files
     }
     plugins {
-        id("com.google.devtools.ksp") version "1.9.24-1.0.20" apply false
+        id("com.google.devtools.ksp") version "2.0.0-1.0.22" apply false
     }
 }
 
