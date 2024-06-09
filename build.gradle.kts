43513d33b4b63f431241ffe481d6bbbfeb2fd47a
Frédéric Torcheux <frederic.torcheux@djit.fr>
2024-06-08 23:40:20 -0400

    [Gradle] Bump Gradle to 8.8 and AGP to 8.4.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
         google()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.3.0")
+        classpath("com.android.tools.build:gradle:8.4.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.24")
         classpath("com.google.gms:google-services:4.3.14")
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.5.3")
